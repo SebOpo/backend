76d8848806f88fead5afb3fcc84818426ea11707
sathoune <michal5bargiel@gmail.com>
2023-01-29 21:19:29 +0100

    Fix circular import in the model by using string instead of a Class.

--- app/components/user/models.py
+++ app/components/user/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
-from app.components.organizations.models import Organization
 from app.db.base_class import Base
 from app.db.utc_convertation import utcnow
 
@@ -27,7 +26,7 @@ class User(Base):
     username = Column(String)
     full_name = Column(String, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
-    organization = Column(Integer, ForeignKey(Organization.id, ondelete="SET NULL"))
+    organization = Column(Integer, ForeignKey("organization.id", ondelete="SET NULL"))
 
     hashed_password = Column(String)
 
