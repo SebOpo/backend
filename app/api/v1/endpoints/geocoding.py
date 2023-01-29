c87d585998d0c867fbd3fb717f71227baf8a5cb1
sathoune <michal5bargiel@gmail.com>
2023-01-29 19:11:21 +0100

    Fix missing properties; remove typing when scope is unused.

--- app/api/v1/endpoints/geocoding.py
+++ app/api/v1/endpoints/geocoding.py
@@ -1,34 +1,27 @@
-from typing import Any
 import logging
+from typing import Any
 
 from fastapi import APIRouter, Security
 
-from app import schemas, models
+from app import schemas
 from app.api.dependencies import get_current_active_user
-from app.utils import geocoding
 from app.core.config import settings
+from app.utils import geocoding
 
 router = APIRouter()
 logger = logging.getLogger(settings.PROJECT_NAME)
 
 
-@router.get('/reverse')
+@router.get("/reverse")
 async def reverse_geocode_location(
-        lat: float,
-        lng: float,
-        geocoder: schemas.GeocoderEnum = "osm",
-        current_user: models.User = Security(
-            get_current_active_user,
-            scopes=['locations:view']
-        )
+    lat: float,
+    lng: float,
+    geocoder: schemas.GeocoderEnum = "osm",
+    current_user=Security(get_current_active_user, scopes=["locations:view"]),
 ) -> Any:
 
-    address_info = geocoding.reverse(
-        lat=lat,
-        lng=lng,
-        geocoding_service=geocoder
-    )
+    address_info = geocoding.reverse(lat=lat, lng=lng, geocoding_service=geocoder)
 
-    logger.debug('Geocoding results : {}'.format(address_info))
+    logger.debug("Geocoding results : {}".format(address_info))
 
     return schemas.OSMGeocodingResults(**address_info)
--- app/api/v1/endpoints/oauth.py
+++ app/api/v1/endpoints/oauth.py
@@ -1,95 +1,65 @@
 from typing import Any, List
 
 from fastapi import APIRouter, Security, Depends, HTTPException
-
 from sqlalchemy.orm import Session
 
-from app import models, schemas
+from app import schemas
 from app.api.dependencies import get_current_active_user, get_db
 from app.crud.crud_oauth import scopes, roles
-
 
 router = APIRouter()
 
 
-@router.post('/roles/create', response_model=schemas.OauthRoleOut)
+@router.post("/roles/create", response_model=schemas.OauthRoleOut)
 async def create_oauth_role(
-        oauth_role: schemas.OauthRoleCreate,
-        current_active_user: models.User = Security(
-            get_current_active_user,
-            scopes=['oauth:create']
-        ),
-        db: Session = Depends(get_db)
+    oauth_role: schemas.OauthRoleCreate,
+    current_active_user=Security(get_current_active_user, scopes=["oauth:create"]),
+    db: Session = Depends(get_db),
 ) -> Any:
 
-    scope_list = scopes.get_by_multi_id(
-        db=db,
-        ids=oauth_role.scope_ids
-    )
+    scope_list = scopes.get_by_multi_id(db=db, ids=oauth_role.scope_ids)
 
     new_role = roles.get_or_create_role(
-        db=db,
-        role=schemas.OauthRole(**oauth_role),
-        scope_list=scope_list
+        db=db, role=schemas.OauthRole(**oauth_role), scope_list=scope_list
     )
 
     return new_role
 
 
-@router.get('/roles/all', response_model=List[schemas.OauthRoleOut])
+@router.get("/roles/all", response_model=List[schemas.OauthRoleOut])
 async def get_all_oauth_roles(
-        current_active_user: models.User = Security(
-            get_current_active_user,
-            scopes=['oauth:read']
-        ),
-        db: Session = Depends(get_db)
+    current_active_user=Security(get_current_active_user, scopes=["oauth:read"]),
+    db: Session = Depends(get_db),
 ) -> Any:
 
     role_list = roles.get_all(db)
     return role_list
 
 
-@router.put('/roles/patch', response_model=schemas.OauthRoleOut)
+@router.put("/roles/patch", response_model=schemas.OauthRoleOut)
 async def patch_oauth_role(
-        role_id: int,
-        oauth_role: schemas.OauthRoleCreate,
-        current_active_user: models.User = Security(
-            get_current_active_user,
-            scopes=['oauth:edit']
-        ),
-        db: Session = Depends(get_db)
+    role_id: int,
+    oauth_role: schemas.OauthRoleCreate,
+    current_active_user=Security(get_current_active_user, scopes=["oauth:edit"]),
+    db: Session = Depends(get_db),
 ) -> Any:
 
-    db_role = roles.get(
-        db=db,
-        model_id=role_id
-    )
+    db_role = roles.get(db=db, model_id=role_id)
     if not db_role:
-        raise HTTPException(
-            status_code=400, detail="Role not found."
-        )
+        raise HTTPException(status_code=400, detail="Role not found.")
 
-    scope_list = scopes.get_by_multi_id(
-        db=db,
-        ids=oauth_role.scope_ids
-    )
+    scope_list = scopes.get_by_multi_id(db=db, ids=oauth_role.scope_ids)
     patched_role = roles.patch(
-        db=db,
-        role=db_role,
-        scope_list=scope_list,
-        name=oauth_role.verbose_name
+        db=db, role=db_role, scope_list=scope_list, name=oauth_role.verbose_name
     )
 
     return patched_role
 
 
-@router.get('/scopes/all', response_model=List[schemas.OauthScopeOut])
+@router.get("/scopes/all", response_model=List[schemas.OauthScopeOut])
 async def get_all_oauth_scopes(
-        current_active_user: models.User = Security(
-            get_current_active_user,
-            scopes=['oauth:read']
-        ),
-        db: Session = Depends(get_db)
+    current_active_user=Security(get_current_active_user, scopes=["oauth:read"]),
+    db: Session = Depends(get_db),
 ) -> Any:
 
     scope_list = scopes.get_all(db)
