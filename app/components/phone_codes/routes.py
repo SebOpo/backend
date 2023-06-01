7cb9dae14e3a5476bdf124e124cba7e92fdf3985
altry104 <altry104@gmail.com>
2023-06-01 16:12:35 +0300

    1 more import fix

--- app/components/phone_codes/routes.py
+++ app/components/phone_codes/routes.py
@@ -4,7 +4,7 @@ import logging
 from fastapi import APIRouter, Depends, HTTPException, Security
 from sqlalchemy.orm import Session
 
-from app.components import phone_codes
+from app.components.phone_codes import schemas, crud
 from app.api.dependencies import get_db, get_current_active_user
 from app.core.config import settings
 
@@ -14,12 +14,12 @@ logger = logging.getLogger(settings.PROJECT_NAME)
 router = APIRouter(prefix="/phone-codes", tags=["phone-codes"])
 
 
-@router.get('/all', response_model=phone_codes.schemas.PhoneCodeOut)
+@router.get('/all', response_model=schemas.PhoneCodeOut)
 async def get_all_phone_codes(
         db: Session = Depends(get_db)
 ) -> Any:
     try:
-        code_list = phone_codes.crud.codes.get_list(db)
+        code_list = crud.codes.get_list(db)
         return code_list
     except Exception as e:
         logger.error('Encountered an error in /phone-codes/all : {}'.format(e))
